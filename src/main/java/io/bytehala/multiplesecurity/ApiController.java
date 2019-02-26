f49ff22b855c0933fc8d07fd44b63d3fdb847bcc
Lemuel Dulfo <lemuel.dulfo@networkinv.com>
2019-02-26 16:03:04 -0700

    Added API endpoints

---
+++ src/main/java/io/bytehala/multiplesecurity/ApiController.java
@@ -0,0 +1,19 @@
+package io.bytehala.multiplesecurity;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"api"})
+public class ApiController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
+
+
+}
